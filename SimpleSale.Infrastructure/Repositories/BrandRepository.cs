b7e0f14ef10c3a0901411307ed2597d9e2b6b0e9
tructran <trungtruc.tran91@gmail.com>
2023-02-21 18:11:10 +0700

    rename project

---
+++ SimpleSale.Infrastructure/Repositories/BrandRepository.cs
@@ -0,0 +1,13 @@
+﻿using SimpleSale.Core.Entities.Catalog;
+using SimpleSale.Core.Repositories;
+using SimpleSale.Infrastructure.Data;
+
+namespace SimpleSale.Infrastructure.Repositories
+{
+    public class BrandRepository : Repository<Brand>, IBrandRepository
+    {
+        public BrandRepository(SimpleSaleDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ SimpleSale.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleSale.Core.Entities.Catalog;
+using SimpleSale.Core.Repositories;
+using SimpleSale.Infrastructure.Data;
+
+namespace SimpleSale.Infrastructure.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(SimpleSaleDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<Category>> GetCategoriesAsync()
+        {
+            return await _dbContext.Categories.ToListAsync();
+        }
+    }
+}
